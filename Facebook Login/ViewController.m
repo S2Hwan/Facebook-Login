0559a089cf2cc9c93a44d1b477818ea14d90a467
Bilal <bilal.arslan1993@hotmail.com>
2014-06-27 10:37:26 +0300

    Inıtial

---
+++ Facebook Login/ViewController.m
@@ -0,0 +1,42 @@
+//
+//  ViewController.m
+//  Facebook Login
+//
+//  Created by Bilal ARSLAN on 26/06/14.
+//
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+-(void)toggleHiddenState:(BOOL)shouldHide;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self toggleHiddenState:YES];
+    [[self lblLoginStatus] setText:@""];
+}
+
+
+-(void)toggleHiddenState:(BOOL)shouldHide
+{
+    self.lblEMail.hidden = shouldHide;
+    self.lblUserName.hidden = shouldHide;
+    self.profilePicture.hidden = shouldHide;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
